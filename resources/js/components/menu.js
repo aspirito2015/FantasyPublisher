2aaf1b8057213d0a50cd5f3a9a15fd4148c79438
aspirito2015 <aspirito2015@gmail.com>
2024-01-27 11:41:23 -0500

    Remove 'WIP' from Standings

--- resources/js/components/menu.js
+++ resources/js/components/menu.js
@@ -15,7 +15,7 @@ class Menu extends HTMLElement {
                     <a class="pure-menu-heading" href="#main">Fantasy Publisher</a>
 
                     <ul class="pure-menu-list">
-                        <li class="pure-menu-item"><a href="#standings" class="pure-menu-link">Standings (WIP)</a></li>
+                        <li class="pure-menu-item"><a href="#standings" class="pure-menu-link">Standings</a></li>
                         <li class="pure-menu-item"><a href="#publishers" class="pure-menu-link">Publishers (WIP)</a></li>
                         <li class="pure-menu-item"><a href="#games" class="pure-menu-link">Games (WIP)</a></li>
                     </ul>
