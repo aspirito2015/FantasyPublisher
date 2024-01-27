9535811c52757dd90ecc6a7dd423195b8aa1bdb0
aspirito2015 <aspirito2015@gmail.com>
2024-01-27 15:23:54 -0500

    Create publisher tables

---
+++ resources/js/components/publishers-table.js
@@ -0,0 +1,81 @@
+// const output = document.querySelector('.output');
+// const url = 'https://docs.google.com/spreadsheets/d/';
+// const ssid = '1pDaqRMLZTKkLolbQIobl3hOkRWbe8-5Yo7WsXKa4wQc';
+// const q1 = '/gviz/tq?';
+// const q2 = 'tqx=out:json';
+var q3 = 'sheet=GameMasterSheet';
+var publishers = [
+    "Anthony",
+    "Brendan",
+    "Chris",
+    "James",
+    "Kyle",
+    "Louisa",
+    "Matt",
+    "Vanessa"
+]
+
+for (let i=0; i < publishers.length; i++) {
+    var query1 = `select D,E,O,P,Q where C contains '${publishers[i]}'`;
+    var q4 = encodeURIComponent(query1);
+    var url1 = `${url}${ssid}${q1}&${q2}&${q3}&tq=${q4}`;
+    
+    //output.textContent = endpoint1;
+    fetch(url1).then(res => res.text()).then(data => {
+        const temp = data.substr(47).slice(0,-2);
+        const json = JSON.parse(temp);
+        let text = `<h3>${publishers[i]}</h3><table class="pub-tab">`;
+        text += doHeaderRow(json);
+        const rows = json.table.rows;
+        rows.forEach(row => {
+            text += doBodyRow(row.c);
+        });
+        text += '</table>';
+        console.log(text)
+        document.getElementById(`pub-table-${i}`).innerHTML = text;
+    })
+
+}
+
+function doHeaderRow(json) {
+    let text = '<tr>';
+    const cols = json.table.cols;
+    text += `<th>${cols[0].label}</th>`;
+    for (let i = 1; i < cols.length; i++) {
+        text += "<th>" + cols[i].label + "</th>";
+    }
+    text += '</tr>';
+    return text;
+}
+
+function doBodyRow(row) {
+    let text = '<tr>';
+    for (let i = 0; i < row.length; i++) {
+        if (!row[i]) {
+            text += `<td></td>`;
+            continue;
+        }
+        let value = row[i].v;
+        //console.log(`${value}: ${typeof value}`);
+        if(typeof value === "string") {
+            if (value.includes("Date")) {
+                console.log(value);
+                value = value.replace("Date(", "");
+                value = value.replace(")", "");
+                let arr = value.split(",");
+                arr.splice(0, 1);
+                arr[0] = parseInt(arr[0]) + 1;
+                value = arr.join("/");
+                console.log(value);
+            }
+        }
+        if(isNumber(value)) value = Math.round(value * 100) / 100;
+        text += `<td>${value}</td>`;
+    }
+    text += '</tr>';
+    return text;
+}
+
+function isNumber(value) {
+    return typeof value === 'number';
+}
